df52239cd6053e27c7d735964be05c27ad76ed79
Artem  Ivanko <a_ivanko@service-voice.com>
2017-01-23 13:16:00 +0200

    1.04 Клиентская часть переведена на: Английский,Португальский. Спасибо за помощь Hellboy (пользователю r4p3)

---
+++ modules/servers/sinusdocker/lang/english.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Hellboy https://r4p3.net/members/hellboy.2849/
+ */
+
+if (!defined("WHMCS"))
+    die("This file cannot be accessed directly");
+
+$_LANG['sinusdocker_musicbot'] = "The Music Boat";
+$_LANG['sinusdocker_musicbot_description'] = "radio multifunctional bot for Teamspeak 3 servers";
+$_LANG['sinusdocker_for_setting_data'] = "To SinusBot settings using the data provided below";
+$_LANG['sinusdocker_data_for_a_control_panel'] = "The data for access to the control panel";
+$_LANG['sinusdocker_link_control_panel'] = "Link to the control panel";
+$_LANG['sinusdocker_login'] = "Login";
+$_LANG['sinusdocker_passwd'] = "Password";
+$_LANG['sinusdocker_log_bot'] = "Log bot";
+$_LANG['sinusdocker_service_deleted'] = "service is removed";
+$_LANG['sinusdocker_service_suspended'] = "service is disabled";
+$_LANG['sinusdocker_error'] = "Unfortunately, something went wrong.";
+$_LANG['sinusdocker_error_message'] = "Error:";
+$_LANG['sinusdocker_go_back_and_try_again'] = "Please go back and try again.";
+$_LANG['sinusdocker_please_contact_support'] = "If the problem persists please contact the support.";
---
+++ modules/servers/sinusdocker/lang/portuguese-br.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+ * @author Hellboy https://r4p3.net/members/hellboy.2849/
+ */
+
+if (!defined("WHMCS"))
+    die("This file cannot be accessed directly");
+
+$_LANG['sinusdocker_musicbot'] = "O Barco Music";
+$_LANG['sinusdocker_musicbot_description'] = "multifuncional bot de rádio para Teamspeak 3 servidores";
+$_LANG['sinusdocker_for_setting_data'] = "Para configurações SinusBot usando os dados fornecidos abaixo";
+$_LANG['sinusdocker_data_for_a_control_panel'] = "Os dados de acesso ao painel de controle";
+$_LANG['sinusdocker_link_control_panel'] = "Link para o painel de controlo";
+$_LANG['sinusdocker_login'] = "Login";
+$_LANG['sinusdocker_passwd'] = "Password";
+$_LANG['sinusdocker_log_bot'] = "bot Log";
+$_LANG['sinusdocker_service_deleted'] = "serviço é removido";
+$_LANG['sinusdocker_service_suspended'] = "serviço é deficiente";
+$_LANG['sinusdocker_error'] = "Infelizmente, algo deu errado.";
+$_LANG['sinusdocker_error_message'] = "Erro:";
+$_LANG['sinusdocker_go_back_and_try_again'] = "Por favor, volte e tente novamente.";
+$_LANG['sinusdocker_please_contact_support'] = "Se o problema persistir, por favor contacte o apoio.";
